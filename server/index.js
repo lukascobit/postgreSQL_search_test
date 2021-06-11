e82b213b2e918de34b7c0bea5bbbb8332dd7bf6d
Lukas <lukyn.odehnal333@gmail.com>
2021-06-11 17:25:27 +0200

    added .gitignore

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
 const express = require("express");
-const app = express()
-const cors = require('cors')
+const app = express();
+const cors = require('cors');
+
+//middleware
+app.use(cors)
+
+app.listen(3000, ()=>{
+    console.log("listening on 3000");
+})
