65722d7b8f7e77cbc4ce15242b8b2339a8429340
Lukas <lukyn.odehnal333@gmail.com>
2021-06-12 10:19:04 +0200

    added sql

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const pool = require("./db")
 
 //middleware
-app.use(cors)
+app.use(cors);
+
+//routes
+app.get("/users", async(req,res)=>{
+    try {
+        const {name} = req.query;
+        const users = await pool.query(`SELECT * FROM users WHERE first_name || ' ' || last_name ILIKE %${name}% `);
+        res.json(users.rows);
+        console.log(users.rows);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 
 app.listen(3000, ()=>{
     console.log("listening on 3000");
